1adade135d82b688f59396f459f7a1d34c838989
EnricoFabiano85 <efabiano.aer@gmail.com>
2025-04-06 15:23:46 -0600

    Compile-time, element-wise operation on multiple vectors

---
+++ ZipVectorTemplates_v2.cpp
@@ -0,0 +1,48 @@
+// from:
+//   https://github.com/DominicJones/snippets/blob/master/Cxx/zip_array.cpp
+// element-wise operation is templated away
+
+#include <array>
+
+
+template<int ...I>
+struct Array : std::array<int, sizeof...(I)>
+{
+  constexpr Array() : std::array<int, sizeof...(I)>{I...} {}
+};
+
+
+auto constexpr multiplies = [](auto ...v){ return (v*...); };
+
+
+template<typename V0, typename ...V>
+struct Zip
+{
+  static consteval auto eval()
+  {
+    auto constexpr zip = [](auto Op, auto v0, auto ...v) {
+      auto result = v0;
+      for (std::size_t i = 0; i != v0.size(); ++i) {
+        result[i] = Op(v0[i], v[i]...);
+      }
+      return result;
+    }(multiplies, V0{}, V{}...);
+    
+    return [&]<auto ...I>(std::index_sequence<I...>) {
+      return Array<zip[I]...>{};
+    }(std::make_index_sequence<zip.size()>{});
+  }
+
+  using type = decltype(eval());
+};
+
+
+using Test = Zip<Array<3,1,2,3>, 
+                 Array<1,2,3,4>, 
+                 Array<2,3,4,5>>::type;
+using Expected = Array<6,6,24,60>;
+
+static_assert(std::same_as<Test, Expected>);
+
+
+int main() {}
